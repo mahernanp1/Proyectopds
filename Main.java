806fe95a101285855e446eb4a6f759fd240d168d
mahernanp1 <159587791+mahernanp1@users.noreply.github.com>
2024-04-25 14:27:18 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,108 +1,116 @@
-import java.awt.*;
-import java.awt.event.*;
-import javax.swing.*;
-import javax.swing.event.*;
+import java.util.Scanner;
 
-public class MyPanel extends JPanel {
-    private JButton boton_aceptar1;
-    private JButton boton_aceptar3;
-    private JButton boton_aceptar2;
-    private JButton boton_aceptar4;
-    private JButton boton_aceptar5;
-    private JTextField texto_nombre;
-    private JTextField texto_edad;
-    private JTextField texto_genero;
-    private JTextField texto_dieta;
-    private JTextField texto_dieta;
-    private JLabel label_nombre;
-    private JLabel label_edad;
-    private JLabel label_genero;
-    private JLabel label_dieta;
-    private JLabel label_horas de sueño;
-    private JLabel label_medicamentos;
-    private JTextField texto_medicamentos;
-    private JButton boton_aceptar6;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-    public MyPanel() {
-        //construct components
-        boton_aceptar1 = new JButton ("aceptar");
-        boton_aceptar3 = new JButton ("aceptar");
-        boton_aceptar2 = new JButton ("aceptar");
-        boton_aceptar4 = new JButton ("aceptar");
-        boton_aceptar5 = new JButton ("aceptar");
-        texto_nombre = new JTextField (5);
-        texto_edad = new JTextField (5);
-        texto_genero = new JTextField (5);
-        texto_dieta = new JTextField (5);
-        texto_dieta = new JTextField (5);
-        label_nombre = new JLabel ("Nombre:");
-        label_edad = new JLabel ("Edad:");
-        label_genero = new JLabel ("Genero:");
-        label_dieta = new JLabel ("Habitos alimenticios:");
-        label_horas de sueño = new JLabel ("Horas de sueño:");
-        label_medicamentos = new JLabel ("Medicamentos:");
-        texto_medicamentos = new JTextField (5);
-        boton_aceptar6 = new JButton ("aceptar");
+    
+        System.out.println("Bienvenido a VitalCheck.");
+        System.out.println("Por favor, ingrese los siguientes datos:");
 
-        //set components properties
-        label_nombre.setToolTipText ("ingrese su nombre");
-        label_edad.setToolTipText ("ingrese su edad");
-        label_genero.setToolTipText ("ingrese su genero");
-        label_dieta.setToolTipText ("ingrese si tiene algun tipo de dieta");
-        label_horas de sueño.setToolTipText ("ingrese sus horas de sueño");
-        label_medicamentos.setToolTipText ("ingrese si ingiere algun medicamento");
+        System.out.print("Peso (en kg): ");
+        double peso = scanner.nextDouble();
 
-        //adjust size and set layout
-        setPreferredSize (new Dimension (944, 574));
-        setLayout (null);
+        System.out.print("Género (M/F): ");
+        char genero = scanner.next().charAt(0);
 
-        //add components
-        add (boton_aceptar1);
-        add (boton_aceptar3);
-        add (boton_aceptar2);
-        add (boton_aceptar4);
-        add (boton_aceptar5);
-        add (texto_nombre);
-        add (texto_edad);
-        add (texto_genero);
-        add (texto_dieta);
-        add (texto_dieta);
-        add (label_nombre);
-        add (label_edad);
-        add (label_genero);
-        add (label_dieta);
-        add (label_horas de sueño);
-        add (label_medicamentos);
-        add (texto_medicamentos);
-        add (boton_aceptar6);
+        System.out.print("Edad: ");
+        int edad = scanner.nextInt();
 
-        //set component bounds (only needed by Absolute Positioning)
-        boton_aceptar1.setBounds (230, 95, 100, 20);
-        boton_aceptar3.setBounds (230, 155, 100, 20);
-        boton_aceptar2.setBounds (230, 125, 100, 20);
-        boton_aceptar4.setBounds (230, 180, 100, 20);
-        boton_aceptar5.setBounds (230, 210, 100, 20);
-        texto_nombre.setBounds (100, 90, 100, 25);
-        texto_edad.setBounds (100, 120, 100, 25);
-        texto_genero.setBounds (100, 150, 100, 25);
-        texto_dieta.setBounds (125, 180, 100, 25);
-        texto_dieta.setBounds (100, 210, 100, 25);
-        label_nombre.setBounds (0, 95, 100, 25);
-        label_edad.setBounds (0, 120, 100, 25);
-        label_genero.setBounds (0, 150, 100, 25);
-        label_dieta.setBounds (0, 180, 120, 25);
-        label_horas de sueño.setBounds (0, 210, 100, 25);
-        label_medicamentos.setBounds (0, 240, 100, 25);
-        texto_medicamentos.setBounds (100, 240, 100, 25);
-        boton_aceptar6.setBounds (230, 240, 100, 20);
-    }
+        System.out.print("¿Hace actividad física regularmente? (Sí/No): ");
+        String actividadFisica = scanner.next();
 
+        System.out.print("¿Consume algún medicamento? (Sí/No): ");
+        String consumeMedicamento = scanner.next();
 
-    public static void main (String[] args) {
-        JFrame frame = new JFrame ("MyPanel");
-        frame.setDefaultCloseOperation (JFrame.EXIT_ON_CLOSE);
-        frame.getContentPane().add (new MyPanel());
-        frame.pack();
-        frame.setVisible (true);
+    
+        System.out.println("\nSeleccione una enfermedad:");
+        System.out.println("1. Resfriado común");
+        System.out.println("2. Infección de oído");
+        System.out.println("3. Influenza");
+        System.out.println("4. Dolor de garganta");
+        System.out.println("5. Infección urinaria");
+        System.out.println("6. Bronquitis aguda");
+        System.out.println("7. Diabetes");
+        System.out.println("8. Cáncer");
+        System.out.println("9. ninguna de las anteriores");
+        System.out.print("Ingrese el número correspondiente a la enfermedad: ");
+        int opcionEnfermedad = scanner.nextInt();
+
+      
+        switch (opcionEnfermedad) {
+            case 1:
+                System.out.println("\nRecomendaciones para el resfriado común:");
+            System.out.println( "1. Tomar líquidos para evitar la fiebre.");
+              System.out.println( "2. Tomar medicamentos para reducir la fiebre.");
+              System.out.println( "3. Mantener una dieta equilibrada y saludable.");
+                                 
+                break;
+            case 2:
+                System.out.println("\nRecomendaciones para la infección de oído:");
+            System.out.println( "1. Tomar líquidos para evitar la inflamación.");  
+              System.out.println( "2. Tomar medicamentos para reducir la inflamación.");
+              
+                break;
+            case 3:
+                System.out.println("\nRecomendaciones para la influenza:");
+            System.out.println( "1. se recetan el oseltamivir o el zanamivir para inhalar para cinco días.");
+               System.out.println( "2. o una dosis de peramivir por vía intravenosa o una dosis de baloxavir oral. ");
+
+               break;
+            case 4:
+                System.out.println("\nRecomendaciones para el dolor de garganta:");
+             System.out.println( "1. Tomar líquidos para evitar la inflamación.");
+             System.out.println( "2. Hazte gárgaras con agua salada. ");
+                System.out.println( "3. Considera las pastillas o los caramelos duros.");
+
+                break;
+            case 5:
+                System.out.println("\nRecomendaciones para la infección urinaria:");
+             System.out.println( "1. Beber mucha agua. El agua ayuda a diluir la orina y expulsar las bacterias.");
+             System.out.println( "2. Evitar las bebidas que puedan irritar la vejiga.");
+            System.out.println( "3. Usar compresas térmicas.");
+
+               break;
+            case 6:
+                System.out.println("\nRecomendaciones para el bronquitis aguda:");
+             System.out.println( "1. Tomar líquidos.");
+             System.out.println( "2. Un humidificador de ambientes puede ayudar. ");
+             System.out.println( "3. Evitar el consumo de tabaco.");
+
+               break;
+            case 7:
+                System.out.println("\nRecomendaciones para el diabetes:");
+             System.out.println( "1. Prepare y coma alimentos saludables.");
+             System.out.println( "2. Evite el consumo de alcohol.");
+             System.out.println( "3. Tome los medicamentos como se los recetaron, aunque se sienta bien..");
+            
+               break;
+            case 8:
+                System.out.println("\nRecomendaciones para el cáncer:");
+             System.out.println( "1. Mantén una dieta balanceada y haz actividad física. ");
+             System.out.println( "2. Consigue atención médica regular. ");
+             System.out.println( "3. Evita el consumo de tabaco.");
+
+               break;
+            
+          
+            default:
+                System.out.println("\nNo se seleccionó una enfermedad establecida.");
+
+        }
+
+    
+        if (opcionEnfermedad < 1 || opcionEnfermedad > 8) {
+            System.out.println("\n¿Desea solicitar una cita personalizada? (Sí/No): ");
+            String solicitudCita = scanner.next();
+            if (solicitudCita.equalsIgnoreCase("Sí")) {
+                System.out.println("\nSe ha solicitado una cita personalizada. Por favor, espere el contacto del médico.");
+            } else {
+                System.out.println("\nContactese con la linea: 3054379543 Gracias por usar VitalCheck.");
+            }
+        }
+
+        scanner.close();
     }
 }
